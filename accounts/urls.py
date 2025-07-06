6bfd7912f04c4a851a0f1bce639b9ad1c2bab518
PC <nuhad7july02@gmail.com>
2025-07-06 21:11:24 +0600

    hi

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('', views.home_view, name='home'),
 ]
