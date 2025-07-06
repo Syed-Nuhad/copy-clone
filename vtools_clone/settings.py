47619bb4dd435055fcdaae8d1f4b2e9c809cbf67
PC <nuhad7july02@gmail.com>
2025-07-07 01:23:49 +0600

    hi

--- vtools_clone/settings.py
+++ vtools_clone/settings.py
@@ -99,6 +99,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
